e0de1c846bd8b204f33fbc4dd771202f04650145
praveen-kumar-007 <praveen.pr105@gmail.com>
2026-05-13 15:37:40 +0530

    Apply frontend formatting updates

--- client/src/utils/apiBase.js
+++ client/src/utils/apiBase.js
@@ -1,6 +1,5 @@
-const LOCAL_API_URL = (import.meta.env.VITE_API_URL || "http://localhost:5000").replace(
-  /\/+$/,
-  "",
-);
+const LOCAL_API_URL = (
+  import.meta.env.VITE_API_URL || "http://localhost:5000"
+).replace(/\/+$/, "");
 
 export const API_URL = import.meta.env.PROD ? "" : LOCAL_API_URL;
